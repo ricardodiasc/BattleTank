c459bae57f7ebbf7bfdbb024c0f0271971702f78
Ricardo Dias Cavalcante <ricardodc@gmail.com>
2016-09-11 20:32:07 -0300

    More attepts

--- Source/BattleTank/TankPlayerController.cpp
+++ Source/BattleTank/TankPlayerController.cpp
@@ -6,16 +6,28 @@
 
 
 void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
 	ATank* Tank = GetControlledTank();
 
 	if (Tank)
 	{
 		UE_LOG(LogTemp, Error, TEXT("No tank for you..."));
 	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Tank Posessed : %s"), *Tank->GetName());
+	}
 
 	UE_LOG(LogTemp, Warning, TEXT("Begin play called"));
 }
 
 ATank* ATankPlayerController::GetControlledTank() const{
-    return Cast<ATank>(GetPawn());
+	APawn* TempPawn = GetControlledPawn();
+	if (TempPawn) {
+		UE_LOG(LogTemp, Error, TEXT("No Pawn for you..."));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("PAWN FOUND!!!"));
+	}
+	
+	return Cast<ATank>(GetPawn());
 }
