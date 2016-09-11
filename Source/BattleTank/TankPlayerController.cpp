3abb087f3883597955938c90018bc90d97eff4ec
Ricardo Dias <ricardodc@gmail.com>
2016-09-10 21:45:15 -0300

    sei de nada...

--- Source/BattleTank/TankPlayerController.cpp
+++ Source/BattleTank/TankPlayerController.cpp
@@ -1,10 +1,23 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+
 
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
 
 
+void ATankPlayerController::BeginPlay(){
+    Super::BeginPlay();
+    
+    ATank* Tank = GetControlledTank();
+    
+    if(Tank){
+        UE_LOG(LogTemp, Error, TEXT("ATank is missing!"));
+        
+    }else{
+        UE_LOG(LogTemp, Warning, TEXT("Possessed player = %s"),Tank->GetFName());
+    }
+    
+}
 
 ATank* ATankPlayerController::GetControlledTank() const{
     return Cast<ATank>(GetPawn());
