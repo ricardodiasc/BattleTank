cf096502fe92b36a70179181f6ae2d7315c61fca
Ricardo Dias Cavalcante <ricardodc@gmail.com>
2016-09-14 22:07:17 -0300

    INitial AI COntroller

---
+++ Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankAIController.h"
+
+
+
+
+void ATankAIController::BeginPlay() {
+	UE_LOG(LogTemp, Warning, TEXT("Begin play AI Controller"));
+	
+	ATank* Tank = GetControlledTankAI();
+	ATank* PlayerTank = GetPlayerControlled();
+
+	if (!Tank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("No AI Tank for you..."));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI tank : %s"), *Tank->GetName());
+	}
+
+	if (!PlayerTank) {
+		UE_LOG(LogTemp, Error, TEXT("AI not found player...."));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("AI Found player :%s"), *PlayerTank->GetName());
+	}
+}
+
+ATank* ATankAIController::GetControlledTankAI()
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank * ATankAIController::GetPlayerControlled()
+{
+	APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+	ATank* PlayerTank = nullptr;
+	if (PlayerController) {
+		PlayerTank = Cast<ATank>(PlayerController->GetPawn());
+	}
+	return PlayerTank;
+}
+
+
+
+
+
